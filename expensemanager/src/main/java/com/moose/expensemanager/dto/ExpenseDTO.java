5aa89c16be2943b9c1b7b32f57f92f1fbb781ab5
milosdamjanovic17 <milos.10001@yahoo.com>
2022-11-11 22:03:21 +0100

    commiting changes

--- expensemanager/src/main/java/com/moose/expensemanager/dto/ExpenseDTO.java
+++ expensemanager/src/main/java/com/moose/expensemanager/dto/ExpenseDTO.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data // Lombok => internally has all getters and setters and toString
 @AllArgsConstructor // Lombok => full fields constructor
 @NoArgsConstructor // Lombok => No Args constructor
-// Lombok haven't been read, so we used getters and setters
+// We'll use Lombok in this class so no need for boilerplate code, getters/setters, NoArg Constructor, FullArgs Constructor, toString etc
 public class ExpenseDTO {
 
 	
@@ -21,83 +21,7 @@ public class ExpenseDTO {
 	private BigDecimal amount;
 	private Date date;
 	private String dateString; // will hold the data parameters as String, will be used in Thymeleaf template only for UI purposes
-	
-	public ExpenseDTO() {
-		
-	}
 
-	public ExpenseDTO(Long id, String expenseId, String name, String description, BigDecimal amount, Date date,
-			String dateString) {
-		this.id = id;
-		this.expenseId = expenseId;
-		this.name = name;
-		this.description = description;
-		this.amount = amount;
-		this.date = date;
-		this.dateString = dateString;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getExpenseId() {
-		return expenseId;
-	}
-
-	public void setExpenseId(String expenseId) {
-		this.expenseId = expenseId;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public BigDecimal getAmount() {
-		return amount;
-	}
-
-	public void setAmount(BigDecimal amount) {
-		this.amount = amount;
-	}
-
-	public Date getDate() {
-		return date;
-	}
-
-	public void setDate(Date date) {
-		this.date = date;
-	}
-
-	public String getDateString() {
-		return dateString;
-	}
-
-	public void setDateString(String dateString) {
-		this.dateString = dateString;
-	}
-
-	@Override
-	public String toString() {
-		return "ExpenseDTO [id=" + id + ", expenseId=" + expenseId + ", name=" + name + ", description=" + description
-				+ ", amount=" + amount + ", date=" + date + ", dateString=" + dateString + "]";
-	}
 	
 
 }
